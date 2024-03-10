ee8764a0ff3ffb809394187535af6c4c3c4b1bbe
imeureka <lky062@naver.com>
2024-03-10 04:56:56 +0000

    [메인] 디벨롭 머지 메인  (#17)
    * init : 초기세팅
    
    * init : 라우팅 설정
    
    * init: 라우팅 공통 헤더 설정
    
    * init: 최상단 폴더 이동
    
    * [도서검색] 검색대 퍼블리싱  (#5)
    
    * feat: 헤더 구현
    
    * feat: 검색창 1차 구현
    
    * feat: book list 구현
    
    * style: 헤더 & 검색 창 수정
    
    * [로그인] 로그인 화면 퍼블리싱 (#7)
    
    * feat: 로그인 화면 구현
    
    * style: 아이콘 추가 로그인 버튼 추가
    
    * [관리자] 관리자 화면 전체 리스트업 + 책 추가 모달창  (#8)
    
    * chore: page 이름 변경
    
    * chore: page 이름 변경
    
    * chore: 주석 제거
    
    * chore: 메타 정보 변경
    
    * feat: admin 책 추가 모달
    
    * [로그인] 로그인 API 구현 (#12)
    
    * feat: 토큰 검사
    
    * feat: 로그인 api 인스턴스 생성
    
    * feat: 로그인 api 구현
    
    * [도서검색] 도서검색 API 및 책 추가 API (#13)
    
    * feat: 도서 검색 기능 구현
    
    * feat: 도서 검색 구현
    
    * feat: 도서 검색 결과 fetch
    
    * chore: 스타일 뒤로 빼기
    
    * feat: 책 추가 api 구현
    
    * feat: 로그아웃 구현
    
    * style: 추가하기 버튼 조정
    
    * feat: http 보안
    
    * feat: user 구분 페이지 라우팅

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -4,6 +4,63 @@ import LoginLogo from '../../assets/login.png';
 import { cleanHeaderInstance } from '../../apis/Client';
 import { useNavigate } from 'react-router-dom';
 
+interface ApiResponseData {
+  result: {
+    id: number;
+    accessToken: string;
+  };
+}
+
+export default function LoginInput() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState<string>(''); // 아이디 입력값을 위한 상태
+  const [password, setPassword] = useState<string>(''); // 비밀번호 입력값을 위한 상태
+
+  const handleLogin = async () => {
+    try {
+      const response = await cleanHeaderInstance.post<ApiResponseData>('/auth', {
+        username: username,
+        password: password,
+      });
+
+      if (response.status === 200) {
+        console.log('로그인 성공', response);
+        localStorage.setItem('accessToken', response.data.result.accessToken);
+        const userId = response.data.result.id;
+        if (userId === 2) {
+          navigate('/admin');
+        } else if (userId === 4) {
+          navigate('/main');
+        } else {
+          console.error('알 수 없는 사용자 ID입니다.');
+        }
+      } else {
+        console.error('로그인 실패');
+      }
+    } catch (error) {
+      console.error('서버 오류:', error);
+    }
+  };
+  return (
+    <LoginWrapper>
+      <Logo src={LoginLogo} alt='logo' />
+      <InputField
+        type='text'
+        placeholder='아이디를 입력하세요'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <InputField
+        type='password'
+        placeholder='비밀번호를 입력하세요'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleLogin}>로그인</Button>
+    </LoginWrapper>
+  );
+}
+
 const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -42,48 +99,3 @@ const Logo = styled.img`
   padding: 5%;
   margin-bottom: 3rem;
 `;
-
-export default function LoginInput() {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState<string>(''); // 아이디 입력값을 위한 상태
-  const [password, setPassword] = useState<string>(''); // 비밀번호 입력값을 위한 상태
-
-  const handleLogin = async () => {
-    try {
-      const response = await cleanHeaderInstance.post('/auth', {
-        username: username,
-        password: password,
-      });
-
-      if (response.status === 200) {
-        console.log('로그인 성공' + response);
-        localStorage.setItem('accessToken', response.data.result.accessToken);
-        navigate('/main');
-      } else {
-        // 로그인 실패 시 처리
-        console.error('로그인 실패');
-      }
-    } catch (error) {
-      console.error('서버 오류:', error);
-    }
-  };
-
-  return (
-    <LoginWrapper>
-      <Logo src={LoginLogo} alt='logo' />
-      <InputField
-        type='text'
-        placeholder='아이디를 입력하세요'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <InputField
-        type='password'
-        placeholder='비밀번호를 입력하세요'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={handleLogin}>로그인</Button>
-    </LoginWrapper>
-  );
-}
