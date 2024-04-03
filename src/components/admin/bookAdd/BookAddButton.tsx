85afa789067f9240ddac925e6e213a3637aeb717
imeureka <lky062@naver.com>
2024-04-03 13:33:47 +0000

    chore: 혜화역 하드코딩 제거

--- src/components/admin/bookAdd/BookAddButton.tsx
+++ src/components/admin/bookAdd/BookAddButton.tsx
@@ -2,6 +2,132 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { instance } from '../../../apis/Client';
 
+export default function BookAddButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+  const [bookInfo, setBookInfo] = useState({
+    name: '',
+    genre: '',
+    location: '',
+    number: 0,
+  });
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) return;
+
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await instance.post('/book/all', formData);
+
+      if (response && response.data) {
+        console.log('파일 업로드 성공');
+      } else {
+        console.error('파일 업로드 실패');
+      }
+    } catch (error) {
+      console.error('파일 업로드 중 오류 발생:', error);
+    }
+  };
+
+  const handleAddBook = async () => {
+    try {
+      const response = await instance.post('/book', bookInfo);
+
+      if (response && response.data) {
+        console.log('책 추가 성공:', response.data);
+      } else {
+        console.error('책 추가 실패');
+      }
+    } catch (error) {
+      console.error('책 추가 중 오류 발생:', error);
+    }
+  };
+
+  const handleSearch = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setBookInfo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <InputWrapper>
+        <ButtonWrapper>
+          <Button onClick={handleSearch}>+</Button>
+        </ButtonWrapper>
+      </InputWrapper>
+
+      {isModalOpen && (
+        <ModalBackground>
+          <ModalContent>
+            <CloseButton onClick={handleCloseModal}>X</CloseButton>
+            <BookInfoWrapper>
+              <label>책 제목</label>
+              <input
+                type='text'
+                placeholder='책 제목 입력'
+                name='name'
+                value={bookInfo.name}
+                onChange={handleInputChange}
+              />
+              <label>장르</label>
+              <input
+                type='text'
+                placeholder='장르 입력'
+                name='genre'
+                value={bookInfo.genre}
+                onChange={handleInputChange}
+              />
+              <label>권수</label>
+              <input
+                type='number'
+                placeholder='최종 권수 입력'
+                name='number'
+                value={bookInfo.number}
+                onChange={handleInputChange}
+              />
+              <label>위치</label>
+              <input
+                type='text'
+                placeholder='위치 입력'
+                name='location'
+                value={bookInfo.location}
+                onChange={handleInputChange}
+              />
+            </BookInfoWrapper>
+            <ButtonAdd onClick={handleAddBook}>추가하기</ButtonAdd>
+            <ButtonExcelWrapper>
+              <ButtonAddFile>
+                엑셀 등록
+                <FileInput type='file' accept='.xlsx, .xls, .csv' onChange={handleFileChange} />
+              </ButtonAddFile>
+              <ButtonAddExcel onClick={handleUpload}> 추가하기</ButtonAddExcel>
+            </ButtonExcelWrapper>
+          </ModalContent>
+        </ModalBackground>
+      )}
+    </>
+  );
+}
+
 const InputWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -79,129 +205,3 @@ const ButtonExcelWrapper = styled.div`
   justify-content: space-between;
   width: 100%;
 `;
-export default function BookAddButton() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [file, setFile] = useState<File | null>(null);
-  const [bookInfo, setBookInfo] = useState({
-    name: '',
-    genre: '',
-    location: '',
-    number: 0,
-  });
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!file) return;
-
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
-
-      const response = await instance.post('/book/all', formData);
-
-      if (response && response.data) {
-        console.log('파일 업로드 성공');
-      } else {
-        console.error('파일 업로드 실패');
-      }
-    } catch (error) {
-      console.error('파일 업로드 중 오류 발생:', error);
-    }
-  };
-
-  const handleAddBook = async () => {
-    try {
-      const response = await instance.post('/book', bookInfo);
-
-      if (response && response.data) {
-        console.log('책 추가 성공:', response.data);
-      } else {
-        console.error('책 추가 실패');
-      }
-    } catch (error) {
-      console.error('책 추가 중 오류 발생:', error);
-    }
-  };
-
-  const handleSearch = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setBookInfo((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  return (
-    <>
-      <InputWrapper>
-        <p>헤화역점</p>
-        <ButtonWrapper>
-          <Button onClick={handleSearch}>+</Button>
-        </ButtonWrapper>
-      </InputWrapper>
-
-      {isModalOpen && (
-        <ModalBackground>
-          <ModalContent>
-            <CloseButton onClick={handleCloseModal}>X</CloseButton>
-            <BookInfoWrapper>
-              <label>책 제목</label>
-              <input
-                type='text'
-                placeholder='책 제목 입력'
-                name='name'
-                value={bookInfo.name}
-                onChange={handleInputChange}
-              />
-              <label>장르</label>
-              <input
-                type='text'
-                placeholder='장르 입력'
-                name='genre'
-                value={bookInfo.genre}
-                onChange={handleInputChange}
-              />
-              <label>권수</label>
-              <input
-                type='number'
-                placeholder='최종 권수 입력'
-                name='number'
-                value={bookInfo.number}
-                onChange={handleInputChange}
-              />
-              <label>위치</label>
-              <input
-                type='text'
-                placeholder='위치 입력'
-                name='location'
-                value={bookInfo.location}
-                onChange={handleInputChange}
-              />
-            </BookInfoWrapper>
-            <ButtonAdd onClick={handleAddBook}>추가하기</ButtonAdd>
-            <ButtonExcelWrapper>
-              <ButtonAddFile>
-                엑셀 등록
-                <FileInput type='file' accept='.xlsx, .xls, .csv' onChange={handleFileChange} />
-              </ButtonAddFile>
-              <ButtonAddExcel onClick={handleUpload}> 추가하기</ButtonAddExcel>
-            </ButtonExcelWrapper>
-          </ModalContent>
-        </ModalBackground>
-      )}
-    </>
-  );
-}
